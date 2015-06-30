416495982c0876d3bf9f180fb10b707130ba46ca
Matthew Han <matthew_han@outlook.com>
2015-06-30 14:53:00 -0700

    modified navbar coloring

---
+++ client/js/helper.js
@@ -0,0 +1,8 @@
+app.filter('range', function(){
+   return function(input,total){
+      total = parseInt(total);
+      for(var i = 1; i<total+1;i++)
+         input.push(i);
+      return input;
+   }
+});
